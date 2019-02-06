54b16a129e6401d4f29dec9462900760b98bae43
NorbertNader <norbertnader@gmail.com>
2019-02-06 16:05:46 -0600

    Test memo

--- src/js/components/container/FormContainer.jsx
+++ src/js/components/container/FormContainer.jsx
@@ -1,30 +1,49 @@
-import React, { Component } from "react";
-import Input from "../presentational/Input.jsx";
-class FormContainer extends Component {
-  constructor() {
-    super();
-    this.state = {
-      seo_title: ""
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange(event) {
-    this.setState({ [event.target.id]: event.target.value });
-  }
-  render() {
-    const { seo_title } = this.state;
-    return (
-      <form id="article-form">
-        <Input
-          text="SEO title"
-          label="seo_title"
-          type="text"
-          id="seo_title"
-          value={seo_title}
-          handleChange={this.handleChange}
-        />
-      </form>
-    );
-  }
+import React, { useState, useEffect, useMemo } from "react";
+
+let memo = [];
+
+export default function App() {
+  const [value, setValue] = useState();
+
+  useEffect(() => {}, [value]);
+
+  memo = useMemo(() => (typeof value === 'number' ? [ ...memo, value ] : memo), [value]);
+
+  const updateUl = () => {
+      setTimeout(() => setValue(0), 100);
+      setTimeout(() => setValue(0), 200);
+      setTimeout(() => setValue(1), 300);
+      setTimeout(() => setValue(1), 400);
+      setTimeout(() => setValue(1), 500);
+      setTimeout(() => setValue(1), 600);
+      setTimeout(() => setValue(2), 700);
+      setTimeout(() => setValue(2), 800);
+      setTimeout(() => setValue(2), 900);
+      setTimeout(() => setValue(2), 1000);
+      setTimeout(() => setValue(2), 1100);
+      setTimeout(() => setValue(2), 1200);
+      setTimeout(() => setValue(2), 1300);
+      setTimeout(() => setValue(2), 1400);
+      setTimeout(() => setValue(3), 1500);
+      setTimeout(() => setValue(3), 1600);
+      setTimeout(() => setValue(3), 1700);
+      setTimeout(() => setValue(3), 1800);
+      setTimeout(() => setValue(4), 1900);
+      setTimeout(() => setValue(4), 2000);
+  };
+
+  // useEffect(() => {
+  //   memo.push(value);
+  // }, [value]);
+
+  return (
+    <>
+      <button onClick={updateUl}>add</button>
+      <ul>
+        {memo.map((item, i) =>
+          <li key={i}>{item}test</li>
+        )}
+      </ul>
+    </>
+  );
 }
-export default FormContainer;
